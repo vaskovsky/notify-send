262c0f1042a7e63a4a272578a07cb1db9cfdc319
Alexey Vaskovsky <alexey@vaskovsky.net>
2016-11-02 14:54:01 +0300

    Added new options.

--- NotifySend.cs
+++ NotifySend.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Drawing;
-using System.Windows.Forms;
-using System.Threading;
-using System.Reflection;
-
-/**
+﻿/**
  * © 2016 Alexey Vaskovsky <alexey@vaskovsky.net>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -20,116 +14,287 @@ using System.Reflection;
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+using System;
+using System.Windows.Forms;
+using System.Reflection;
+
 namespace Vaskovsky
 {
-	/** NotifySend notify-send is a tool that displays pop-up desktop notifications. */
-	class NotifySend : ApplicationContext
-	{
-		private const int DefaultTimeout = 10000;
-		private const string IconFile = "notify-send.ico";
-		private const string Title = "notify-send for Windows";
-		private const string Usage = @"Usage: 
-  notify-send [-i ICON] [-t TIMEOUT] ""TITLE"" ""MESSAGE"" 
-© 2016 Alexey Vaskovsky 
+    /** NotifySend notify-send is a tool that displays pop-up desktop notifications. */
+    class NotifySend : ApplicationContext
+    {
+        private const string Version = "notify-send for Windows 2.1.0";
+		private const int DefaultTimeout = 5000;
+        private const string DefaultAppName = "notify-send for Windows";
+        private const string DefaultTitle = DefaultAppName;
+        private const string Usage = @"Usage:
+  notify-send [-i ICON] ""TITLE"" ""MESSAGE""
+© 2016 Alexey Vaskovsky
 For more information visit http://vaskovsky.net/notify-send
 ";
-		private NotifyIcon Notification;
-		private int Timeout;
+        private NotifyIcon Notification;
+        private bool _ExitOnClose = false;
+        private bool IsRunning = true;
 
-		/** Creates new notification.
-         * @param timeout is a timeout in milliseconds.
-         */
-		public NotifySend (string title, string text, ToolTipIcon icon, int timeout)
-		{
-			Timeout = timeout;
+        /** Specifies the debug mode. */
+        public bool Debug = false;
 
-			Notification = new NotifyIcon ();
-			Notification.Icon = Icon.ExtractAssociatedIcon (Assembly.GetExecutingAssembly ().Location); 
-			Notification.Text = Title;
+        /**
+		 * Specifies the timeout in milliseconds
+		 * at which to expire the notification.
+		 */
+        public int Timeout = DefaultTimeout;
 
-			Notification.BalloonTipIcon = icon;
-			Notification.BalloonTipTitle = title;
-			Notification.BalloonTipText = text;
+        /** Specifies the message title. */
+        public string Title
+        {
+            get { return Notification.BalloonTipTitle; }
+            set { Notification.BalloonTipTitle = value; }
+        }
 
-			Notification.BalloonTipClosed += new EventHandler (this.Exit);
-			Notification.BalloonTipClicked += new EventHandler (this.Exit);
-			Notification.Click += new EventHandler (this.Exit);
-		}
+        /** Specifies the message text. */
+        public string Message
+        {
+            get { return Notification.BalloonTipText; }
+            set { Notification.BalloonTipText = value; }
+        }
 
-		/** Displays this notification. */
-		public void Show ()
-		{
-			Notification.Visible = true;
-			Notification.ShowBalloonTip (Timeout);
-		}
+        /** Specifies the application name for the icon. */
+        public string AppName
+        {
+            get { return Notification.Text; }
+            set { Notification.Text = value; }
+        }
 
-		private void Show (object sender, EventArgs e)
-		{
-			this.Show ();
-		}
+        /** Specifies an icon to display. */
+        public ToolTipIcon Icon
+        {
+            get { return Notification.BalloonTipIcon; }
+            set { Notification.BalloonTipIcon = value; }
+        }
 
-		private void Exit (object sender, EventArgs e)
-		{
-			Notification.Visible = false;
-			this.ExitThread ();
-		}
+        /**
+		 * If this property is set to true,
+		 * it will exit thread when you close the notification.
+		 *
+		 * Default: false.
+		 */
+        public bool ExitOnClose
+        {
+            get { return _ExitOnClose; }
+            set
+            {
+                if (value && !_ExitOnClose)
+                {
+                    Notification.BalloonTipClosed += this.Exit;
+                    Notification.BalloonTipClicked += this.Exit;
+                    Notification.Click += this.Exit;
+                }
+                if (!value && _ExitOnClose)
+                {
+                    Notification.BalloonTipClosed -= this.Exit;
+                    Notification.BalloonTipClicked -= this.Exit;
+                    Notification.Click -= this.Exit;
+                }
+                _ExitOnClose = value;
+            }
+        }
 
-		/** Parses arguments and runs this application. */
-		static void Main (string[] args)
-		{
-			string title = null;
-			var text = "";
-			var icon = ToolTipIcon.None;
-			int timeout = DefaultTimeout;
+        /** Creates new notification. */
+        public NotifySend()
+        {
+            Notification = new NotifyIcon();
+            Notification.Icon = System.Drawing.Icon.ExtractAssociatedIcon(
+                Assembly.GetExecutingAssembly().Location);
+            AppName = DefaultAppName;
+            Title = DefaultTitle;
+            Message = "";
+            Icon = ToolTipIcon.Info;
+            Notification.BalloonTipClosed += new EventHandler(this.Hide);
+            Notification.BalloonTipClicked += new EventHandler(this.Hide);
+            Notification.Click += new EventHandler(this.Hide);
+        }
 
-			for (int i = 0; i < args.Length; i++) {
-				if (args [i] == "-i") {
-					i++;
-					if (i < args.Length) {
-						if (args [i] == "info") {
-							icon = ToolTipIcon.Info;
-						} else if (args [i] == "important") {
-							icon = ToolTipIcon.Warning;
-						} else if (args [i] == "error") {
-							icon = ToolTipIcon.Error;
-						}
-					}
-				} else if (args [i] == "-t") {
-					i++;
-					if (i < args.Length) {
-						try {
-							timeout = Int32.Parse (args [i]);
-						} catch (Exception) {
-						}
-					}
-				} else if (title == null) {
-					title = args [i];
-				} else {
-					if (text != "") {
-						text += " ";
-					}
-					text += args [i];
-				}
-			}
+        /** Creates new notification and sets ExitOnClose = true. */
+        [Obsolete("Use NotifySend() and properties.")]
+        public NotifySend(
+            string title, string message, ToolTipIcon icon, int timeout
+        ) : this()
+        {
+            Title = title;
+            Message = message;
+            Icon = icon;
+            Timeout = timeout;
+            ExitOnClose = true;
+        }
 
-			if (text == "") {
-				title = Title;
-				text = Usage;
-			}
+        /** Displays this notification. */
+        public void Show()
+        {
+            if (Debug)
+                Console.WriteLine(Timestamp() + " NotifySend.Show");
+            Notification.Visible = true;
+            Notification.ShowBalloonTip(Timeout);
+        }
 
-			if (icon == ToolTipIcon.None) {
-				icon = ToolTipIcon.Info;
-			}
+        private void Hide(object sender, EventArgs e)
+        {
+            if (Notification.Visible)
+            {
+                if (Debug)
+                    Console.WriteLine(Timestamp() + " NotifySend.Hide");
+                Notification.Visible = false;
+            }
+        }
 
-			/* Testing
-			Console.WriteLine ("Icon: " + icon);
-			Console.WriteLine ("Timeout: " + timeout);
-			Console.WriteLine ("Title: " + title);
-			Console.WriteLine ("Message: " + text);/**/
+        private void Exit(object sender, EventArgs e)
+        {
+            if (IsRunning)
+            {
+                IsRunning = false;
+                if (Debug)
+                    Console.WriteLine(Timestamp() + " NotifySend.Exit");
+                this.ExitThread();
+            }
+        }
 
-			var notifySend = new NotifySend (title, text, icon, timeout);
-			notifySend.Show ();
-			Application.Run (notifySend);
-		}
-	}
+        private static string Timestamp()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+        }
+
+        /** Parses arguments and runs this application. */
+        static void Main(string[] args)
+        {
+            try
+            {
+                var notifySend = new NotifySend();
+                notifySend.ExitOnClose = true;
+                var message = "";
+                var warning = "";
+                bool hasTitle = false;
+                bool optionsEnd = false;
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (!optionsEnd)
+                    {
+                        if (args[i] == "-i")
+                        {
+                            i++;
+                            if (i < args.Length)
+                            {
+                                if (args[i] == "info")
+                                {
+                                    notifySend.Icon = ToolTipIcon.Info;
+                                }
+                                else if (args[i] == "important")
+                                {
+                                    notifySend.Icon = ToolTipIcon.Warning;
+                                }
+                                else if (args[i] == "error")
+                                {
+                                    notifySend.Icon = ToolTipIcon.Error;
+                                }
+                            }
+                            continue;
+                        }
+                        if (args[i] == "-t")
+                        {
+                            i++;
+                            if (i < args.Length)
+                            {
+                                try
+                                {
+                                    notifySend.Timeout = Int32.Parse(args[i]);
+                                }
+                                catch (Exception error)
+                                {
+                                    warning = "NotifiSend.Timeout: " +
+                                        error.Message + " [" + error.GetType() + "]";
+                                }
+                            }
+                            continue;
+                        }
+                        if (args[i] == "-a")
+                        {
+                            i++;
+                            if (i < args.Length)
+                            {
+                                notifySend.AppName = args[i];
+                            }
+                            continue;
+                        }
+                        if (args[i] == "--debug")
+                        {
+                            notifySend.Debug = true;
+                            continue;
+                        }
+                        if (args[i] == "-u" || args[i] == "-c" || args[i] == "-h")
+                        {
+                            i++;
+                            continue;
+                        }
+						if (args[i] == "-v")
+                        {
+                            Console.WriteLine(Version);
+                            return;
+                        }
+						if (args[i] == "-?")
+                        {
+                            Console.WriteLine(Usage);
+                            return;
+                        }
+                        if (args[i] == "--")
+                        {
+                            optionsEnd = true;
+                            continue;
+                        }
+                    }
+
+                    if (!hasTitle)
+                    {
+                        notifySend.Title = args[i];
+                        hasTitle = true;
+                        continue;
+                    }
+
+                    if (message != "")
+                    {
+                        message += " ";
+                    }
+                    message += args[i];
+                }
+
+                if (message == "" && !hasTitle)
+                {
+                    notifySend.Title = DefaultTitle;
+                    notifySend.Message = Usage;
+                }
+                else
+                {
+                    notifySend.Message = message;
+                }
+                if (warning != "")
+                    Console.Error.WriteLine(warning);
+                if (notifySend.Debug)
+                {
+                    Console.WriteLine(Timestamp() + " NotifySend.Init");
+                    Console.WriteLine("\tAppName: " + notifySend.AppName);
+                    Console.WriteLine("\tTitle: " + notifySend.Title);
+                    Console.WriteLine("\tMessage: " + notifySend.Message);
+                    Console.WriteLine("\tIcon: " + notifySend.Icon);
+                    Console.WriteLine("\tTimeout: " + notifySend.Timeout);
+                }
+                notifySend.Show();
+                Application.Run(notifySend);
+            }
+            catch (Exception error)
+            {
+                Console.Error.WriteLine("NotifySend: " + error.Message +
+                    " [" + error.GetType() + "]");
+            }
+        }
+    }
 }
--- README.md
+++ README.md
@@ -4,61 +4,43 @@ notify-send is a tool that displays pop-up desktop notifications.
 
 ## Building
 
-1. Open `notify-send.sln` in **Visual Studio** or **MonoDevelop**.
+1. Open `notify-send.sln` in **Visual Studio Community 2015+** or **MonoDevelop**.
 
 2. Select **Release** from the **Solution Configuration** drop-down list,
-   which is on the **Standard** toolbar
-   (ignore this step in **Visual Studio Express**).
+   which is on the **Standard** toolbar.
 
 3. On the **Build** menu, click **Build**.
 
-You can find `notify-send.exe` in `bin/Release` folder
-(or in `bin/Debug` if you are using **Visual Studio Express**).
+You can find `notify-send.exe` in `bin/Release` folder.
 
-## Usage
+## Documentation
 
-		notify-send [-i ICON] [-t TIMEOUT] "TITLE" ["MESSAGE"]
+[Usage](http://vaskovsky.net/notify-send/)
 
-### Options
+[Troubleshooting](http://vaskovsky.net/notify-send/troubleshooting.html)
 
-* `ICON`: specifies an icon to display. The possible values are:
-`info` | `important` | `error`. Default: info.
+[Changelog](http://vaskovsky.net/notify-send/changelog.html)
 
-* `TIMEOUT`: specifies the timeout in milliseconds at which to
-expire the notification. Default: 10000
+## Downloads
 
-* `TITLE`: message title.
-
-* `MESSAGE`: message text.
-
-### Examples
-
-		notify-send "My Message"
-
-		notify-send "Title" "Message"
-
-		notify-send -i error "Error" "File not found"
-
-		notify-send -i important "Attention!" "You should upgrade your software"
-
-## License LGPL v3
-
-Copyright © 2015 Alexey Vaskovsky
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3.0 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Lesser General Public License for more details.
-
-## See also
-
-[notify-send Documentation](http://vaskovsky.net/notify-send/)
+[Download precompiled binary](http://vaskovsky.net/notify-send/notify-send.zip)
 
 [Download Visual Studio Express](https://www.visualstudio.com/ru-ru/products/visual-studio-express-vs.aspx)
 
 [Download MonoDevelop](http://www.monodevelop.com/download/)
+
+## License
+
+Copyright © 2012-2016 Alexey Vaskovsky <alexey@vaskovsky.net>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the [GNU Lesser General Public License][1] for more details.
+
+[1]: http://vaskovsky.net/notify-send/license.html
